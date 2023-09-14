9896a13202bd0fe175f68681dd876063d2378847
ahoang2502 <athoang2502@gmail.com>
2023-09-14 18:28:49 +1000

    get all msg

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { Member } from "@prisma/client";
+import { Fragment } from "react";
+import { Loader2, ServerCrash } from "lucide-react";
 
 import ChatWelcome from "./ChatWelcome";
+import { useChatQuery } from "@/hooks/useChatQuery";
+import { MessageWithMemberWithProfile } from "@/types";
 
 interface ChatMessagesProps {
 	name: string;
@@ -27,10 +31,50 @@ const ChatMessages = ({
 	paramValue,
 	type,
 }: ChatMessagesProps) => {
+	const queryKey = `chat:${chatId}`;
+
+	const { data, fetchNextPage, isFetchingNextPage, status, hasNextPage } =
+		useChatQuery({
+			queryKey,
+			apiUrl,
+			paramKey,
+			paramValue,
+		});
+
+	if (status === "loading")
+		return (
+			<div className="flex flex-col flex-1 justify-center items-center">
+				<Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+				<p className="text-xs text-zinc-500 dark:text-zinc-400">
+					Loading messages...
+				</p>
+			</div>
+		);
+
+	if (status === "error")
+		return (
+			<div className="flex flex-col flex-1 justify-center items-center">
+				<ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+				<p className="text-xs text-zinc-500 dark:text-zinc-400">
+					Something went wrong. Please try again later!
+				</p>
+			</div>
+		);
+
 	return (
 		<div className="flex-1 flex flex-col py-4 overflow-y-auto ">
 			<div className="flex-1" />
 			<ChatWelcome type={type} name={name} />
+
+			<div className="flex flex-col-reverse mt-auto ">
+				{data?.pages?.map((group, i) => (
+					<Fragment key={i}>
+						{group.items.map((message: MessageWithMemberWithProfile) => (
+							<div key={message.id}>{message.content}</div>
+						))}
+					</Fragment>
+				))}
+			</div>
 		</div>
 	);
 };
