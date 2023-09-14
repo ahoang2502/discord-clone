2f5ba2d9d9af1ee2f528979bb07b388c05f13618
ahoang2502 <athoang2502@gmail.com>
2023-09-14 14:39:25 +1000

    msg-input api

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/ChatHeader";
+import ChatInput from "@/components/chat/ChatInput";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
@@ -40,6 +41,15 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 				name={channel.name}
 				serverId={channel.serverId}
 				type="channel"
+			/>
+
+			<div className="flex-1">Future Messages</div>
+
+			<ChatInput
+				name={channel.name}
+				type="channel"
+				apiUrl="/api/socket/messages"
+				query={{ channelId: channel.id, serverId: channel.serverId }}
 			/>
 		</div>
 	);
